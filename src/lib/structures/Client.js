4c5fe21158b76297453eb1d46bd18533f289ac3c
Santosh <bsantosh909@gmail.com>
2019-05-12 20:47:13 +0545

    Semantic versioning
    https://semver.org/#how-should-i-deal-with-revisions-in-the-0yz-initial-development-phase

--- src/lib/structures/Client.js
+++ src/lib/structures/Client.js
@@ -3,13 +3,13 @@ const { Client } = require('klasa');
 /**
  * The extended Klasa Client to handle next level features.
  * @extends {KlasaClient}
- * @since 0.0.1
+ * @since 0.1.0
  */
 class PaulClient extends Client {
 
 	/**
 	 * Invite link to the support server of the bot.
-	 * @since 0.0.1
+	 * @since 0.1.0
 	 * @readonly
 	 */
 	get supportServer() {
